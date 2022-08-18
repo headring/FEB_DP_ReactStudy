0936c0623cee161b9315352fe3356a47bfb4c6c8
headring <myhakiki@gmail.com>
2022-08-18 18:16:15 +0900

    SPA 업데이트

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,19 +1,25 @@
 import NumBody from "./NumBase/NumBody";
 import {Routes, Route, Link } from 'react-router-dom'
+import RPCBody from "./RockPaperScissors/RPCBody";
+import LotBody from "./Lottery/LotBody";
 
 export default function App() {
   return (
     <>
       <div> 
         <div>
-          <Link to="/" >Home Sweet Home</Link>
+          <Link to="/" > Main </Link>
+          <Link to="/NumBase" element={<NumBody />}> NumBase </Link>
+          <Link to="/RockPaperScissors" element={<RPCBody />}> RockPaperScissors </Link>
+          <Link to="/Lottery" element={<LotBody />}> Lottery </Link>
         </div>
-        <Link to="/NumBase" element={<NumBody />}>NumBase</Link>
       </div>
     
       <Routes>
         {/* <Route path="/" /> */}
         <Route path="/NumBase" element={<NumBody />}/>
+        <Route path="/RockPaperScissors" element={<RPCBody />} />
+        <Route path="/Lottery" element={<LotBody />} />
       </Routes>
     </>
     
