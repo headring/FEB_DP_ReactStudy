22ab7c73f7fe9bd099b9df1f05a5f6ad72e084c4
headring <myhakiki@gmail.com>
2022-08-21 13:00:55 +0900

    스타일드 주석 작성 및 게임 컴포넌트 작성

--- using_type/src/App.tsx
+++ using_type/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MultiTable from './1.1-Multitable/MultiTable';
+import WordChain from './1.2-WordChain/WordChain';
+
+// mvp css 자동적으로 최소한의 스타일을 정해줌
+
+// styled component
+/*
+변수 = styled.<htmltag>`
+  style 작성
+`;
+*/
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MultiTable />
+      <div>
+        <WordChain />
+      </div>
     </div>
   );
 }
