e94d43853f84a34265557522070b1cf47ebf3f64
headring <myhakiki@gmail.com>
2022-08-23 22:04:52 +0900

    스타일드 컴포넌트  적용

--- jsx/src/Sidebar.js
+++ jsx/src/Sidebar.js
@@ -1,16 +1,31 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Styling = styled.div`
+  display: block;
+`
 
 export default function Sidebar() {
   return (
-    <section className='sidebar'> 
-      <ul>
+    <Styling> 
+      <div>
         <Link to="/" > Main </Link>
+      </div>
+      <div>
         <Link to="/MultiTable"> 1.1-MultiTable </Link>
+      </div>
+      <div>
         <Link to="/WordChain"> 1.2-WordChain </Link>
+      </div>
+      <div>
         <Link to="/NumBase"> 2.NumBase </Link>
+      </div>
+      <div>
         <Link to="/RockPaperScissors"> 3.1-RockPaperScissors </Link>
+      </div>
+      <div>
         <Link to="/Lottery"> 3.2-Lottery </Link>
-      </ul>
-    </section>
+      </div>
+    </Styling>
   )
 }
