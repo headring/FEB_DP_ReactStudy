2cc7351c7e6803051363abccec313390273b0b1f
headring <myhakiki@gmail.com>
2022-09-11 18:03:30 +0900

    import layout component

--- vanilaGatsby-TS/src/pages/about.js
+++ vanilaGatsby-TS/src/pages/about.js
@@ -1,14 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
-      <h1>About Me</h1>
+    <Layout pageTitle="About Me" children="there">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-      <Link to='/'>Go To Index</Link>
+    </Layout>
     </main>
   )
 }
